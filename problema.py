88b06e506897b7ee5410485889e7f338709fe503
Marchis Tania <marchis.tania@yahoo.com>
2021-10-18 14:59:54 +0300

    test

---
+++ problema.py
@@ -0,0 +1,5 @@
+def printMenu():
+    print("1.Găsește ultimul număr prim mai mic decât un număr dat.")
+    print("2.Determină dacă un număr dat este palindrom.")
+    print("3.Determină dacă un număr este superprim: dacă toate prefixele sale sunt prime.")
+    print("Pentru a inchide, introdu x")
